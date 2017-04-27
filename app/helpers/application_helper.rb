fd6081691f61784761e885b679a9bab56fd5228f
Stacy Talbot <stacy.talbot@unep-wcmc.org>
2017-04-27 10:17:31 +0100

    Remove the get_article_types helper and move request into news_and_documents view file.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,4 @@
 module ApplicationHelper
-  def get_article_types
-    ArticleType.all
-  end
-
   def active_link_to(text, path, options={})
     options[:class] ||= ""
     options[:class] << " active" if current_page?(path)
