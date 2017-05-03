0297988a7d5fc6cbd948485a11b6027d8779c639
Spencer Dixon <spencerlloyddixon@gmail.com>
2017-05-03 13:19:26 +0100

    Added file metadata to the bottom of article, automatically calculates size, name, format and language is taken from the cms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,25 @@ module ApplicationHelper
 
     link_to(text, path, options)
   end
+
+  def file_extension_for(file)
+    File.extname(file.url).gsub(".", "").upcase
+  end
+
+  def file_name_for(file)
+    File.basename(file.original_filename, File.extname(file.url)).titleize
+  end
+
+  def file_size_for(file, decimal_places=2)
+    format = file.size < 1_000_000 ? :kb : :mb
+
+    case format
+    when :kb
+      size = file.size.to_f / 1024
+    when :mb
+      size = file.size.to_f / 1024 / 1024
+    end
+
+    size.round(decimal_places).to_s + format.to_s
+  end
 end
