1dce72f1b0136ae31d1a7278c2843f109ebbb4fc
Spencer Dixon <spencerlloyddixon@gmail.com>
2017-05-08 12:04:05 +0100

    Working on social media meta tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -30,4 +30,8 @@ module ApplicationHelper
   def format_url_safe(text)
     CGI.escape(text)
   end
+
+  def yield_or_default(symbol, default)
+    yield(symbol).present? ? yield(symbol) : default
+  end
 end
