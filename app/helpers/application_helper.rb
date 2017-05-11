5bdf921f1b3987dab55cbd46257a27d77fcf6f39
Stacy Talbot <stacy.talbot@unep-wcmc.org>
2017-05-11 11:23:46 +0100

    Refactor any_fields_present helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,22 +31,12 @@ module ApplicationHelper
     ERB::Util.url_encode(text)
   end
 
-  def any_fields_present(type, total)
-    if type == 'file'
-
-      total.times do |n|
-        return true if cms_block_content("file_#{n + 1}".to_sym).try(:file)&.present?
-      end
-
-    else
-
-      total.times do |n|
-        return true if cms_block_content("url_#{n + 1}".to_sym)&.present?
-      end
-
-    end
-
-    return false
+  def any_fields_present(type, total=5)
+    file_fields_present = -> (n) { cms_block_content("file_#{n + 1}".to_sym).try(:file)&.present? }
+    url_fields_present = -> (n) { cms_block_content("url_#{n + 1}".to_sym)&.present? }
+    any_data_for_field = type == 'file' ? file_fields_present : url_fields_present
+    
+    (1..total).any? { |n| any_data_for_field[n] }
   end
-
 end
+
